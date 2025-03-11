ee9c19363ffc0287883b554a59514729ec0a8f00
fox-taisei-murata <taisei.murata@fox-hound.jp>
2025-03-11 14:17:32 +0900

    ユーザー編集ページの作成by村田

--- app/users/[id]/edit/page.tsx
+++ app/users/[id]/edit/page.tsx
@@ -3,23 +3,32 @@
 "use client"; // クライアントコンポーネントとしてマーク
 
 import React from "react";
-import EditUserForm from "../../../../components/EditUserForm";
 import { useParams } from "next/navigation";
 import { Typography, Box } from "@mui/material";
+import { useRouter } from "next/navigation";
+import EditUserForm from "../../../../components/EditUserForm";
 
 // TODO: URLパラメータからユーザーIDを取得し、EditUserFormコンポーネントに渡す
 const EditUserPage: React.FC = () => {
+
+  const { id } = useParams();
+  const router = useRouter();
 
   // ユーザーIDが取得できていない場合はnullを返す
   if (!id || Array.isArray(id)) {
     return <Typography>ユーザーIDが無効です。</Typography>;
   }
 
+  const handleSuccess = () => {
+    router.push("/users"); 
+  };
+
   return (
     <Box sx={{ mt: 4 }}>
       <Typography variant="h4" gutterBottom>
         ユーザー編集
       </Typography>
+      <EditUserForm userId={Number(id)} onSuccess={handleSuccess} />
     </Box>
   );
 };
