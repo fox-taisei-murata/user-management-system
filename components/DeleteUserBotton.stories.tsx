c11c9d3768b41f5ea3151741cdf87a10912f1fd3
fox-taisei-murata <taisei.murata@fox-hound.jp>
2025-03-12 11:26:48 +0900

    DeleteUserButtonのストーリー作成by村田

---
+++ components/DeleteUserBotton.stories.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import DeleteUserButton from "./DeleteUserButton";
+
+// TODO: メタデータ
+const meta: Meta<typeof DeleteUserButton> = {
+    title: "Components/DeleteUserButton",
+    component: DeleteUserButton,
+};
+
+export default meta;
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof DeleteUserButton>;
+
+export const Default: Story = {
+    args: {
+        userId: 1,
+        onDelete: () => { },
+    },
+};
--- components/DeleteUserButton.tsx
+++ components/DeleteUserButton.tsx
@@ -11,26 +11,22 @@ interface DeleteUserButtonProps {
 }
 
 const DeleteUserButton: React.FC<DeleteUserButtonProps> = ({ userId, onDelete }) => {
-  const [Deleting, setDeleting] = useState(false);
 
   const handleDelete = async () => {
     if (!confirm('本当にこのユーザーを削除しますか？')) return;
 
-    setDeleting(true);
     try {
       await softDeleteUser(userId.toString()); 
       onDelete(userId); 
     } catch (error) {
       console.error('削除に失敗しました', error);
       alert('削除に失敗しました');
-    } finally {
-      setDeleting(false);
     }
   };
 
   return (
-    <Button variant="contained" color="error" onClick={handleDelete} disabled={Deleting}>
-      {Deleting ? "削除中..." : "ユーザーを削除"}
+    <Button variant="contained" color="error" onClick={handleDelete} >
+      ユーザーを削除
     </Button>
   );
 };
